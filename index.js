69cb7de93f8f2ae66f0c6fec7453912e7e45c898
cair71 <cairzz222@gmail.com>
2021-05-25 08:15:01 +0100

    removed 'reject' param from delay promise function

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-var before = 'LAST_MESSAGE_ID'; // enter Last id of messages.
-clearMessages = function(){
-    const authToken = document.body.appendChild(document.createElement`iframe`).contentWindow.localStorage.token.replace(/"/g, "");
-    const channel = window.location.href.split('/').pop();
-    const baseURL = `https://discord.com/api/channels/${channel}/messages`;
-    const headers = {"Authorization": authToken };
-    let clock = 0;
-    let interval = 500;
-    function delay(duration) {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => resolve(), duration);
-        });
-    }
-    fetch(baseURL + '?before=' + before + '&limit=100', {headers})
-        .then(resp => resp.json())
-        .then(messages => {
-        return Promise.all(messages.map((message) => {
-            before = message.id;
-            return delay(clock += interval).then(() => fetch(`${baseURL}/${message.id}`, {headers, method: 'DELETE'}));
-        }));
-    }).then(() => clearMessages());
-}
+var before = "LAST_MESSAGE_ID"; // enter the last ID of message
+
+clearMessages = function () {
+  const authToken = document.body
+    .appendChild(document.createElement`iframe`)
+    .contentWindow.localStorage.token.replace(/"/g, "");
+  const channel = window.location.href.split("/").pop();
+  const baseURL = `https://discord.com/api/channels/${channel}/messages`;
+  const headers = { Authorization: authToken };
+  let clock = 0;
+  let interval = 500;
+  function delay(duration) {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(), duration);
+    });
+  }
+  fetch(baseURL + "?before=" + before + "&limit=100", { headers })
+    .then((resp) => resp.json())
+    .then((messages) => {
+      return Promise.all(
+        messages.map((message) => {
+          before = message.id;
+          return delay((clock += interval)).then(() =>
+            fetch(`${baseURL}/${message.id}`, { headers, method: "DELETE" })
+          );
+        })
+      );
+    })
+    .then(() => clearMessages());
+};
+
 clearMessages();
